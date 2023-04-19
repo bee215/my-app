a051485b25f53ccdfde45f0fe4565a43065c372a
bee215 <sberindaniel74@gmail.com>
2023-04-19 09:54:56 +0530

    sd

---
+++ src/berin.js
@@ -0,0 +1,14 @@
+import React,{Component}from 'react';
+
+class SSS extends Component{
+    render(){
+      return(
+        <div classname ="1"><h1>hello</h1><br></br>
+        
+      <big><button><h1>L--->new</h1></button></big> 
+     </div>
+        
+      );
+    }
+  }
+  export default SSS;
